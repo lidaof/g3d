dc92cd38c0763bd6f47f2f97eaa4227088f29d97
Daofeng Li <lidaof@gmail.com>
2019-11-05 16:26:08 -0600

    first ar

--- g3dv/src/views/ARdemo.vue
+++ g3dv/src/views/ARdemo.vue
@@ -1,49 +1,90 @@
 <template>
-   <!-- <a-scene>
-      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-      <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-      <a-sky color="#ECECEC"></a-sky>
-    </a-scene> -->
-    <a-scene embedded arjs>
-  	<a-marker preset="hiro">
-            <!-- <a-box position='0 0.5 0' material='color: yellow;'></a-box> -->
-            <a-entity main></a-entity>
-  	</a-marker>
-  	<a-entity camera></a-entity>
-    </a-scene>
+  <a-scene embedded arjs="sourceType: webcam;" v-if="dataReady">
+    <a-entity mymesh></a-entity>
+    <a-marker-camera preset="hiro"></a-marker-camera>
+  </a-scene>
 </template>
 
 <script>
-import * as THREE from 'three'
+import G3dFile from 'g3djs'
+import { getSplines } from '@/components/Tube'
+const AFRAME = window.AFRAME
+const THREE = AFRAME.THREE // very important
 export default {
-    name: 'ARdemo',
-    data(){
-        return {
-            main: new THREE.Object3D()
-        }
-    },
-    methods: {
-        init(){
-            var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const mesh = new THREE.Mesh( geometry, material );
-this.main.add(mesh)
-        }
-    },
-    created(){
-        this.init()
-        console.log(this.main)
-        AFRAME.registerComponent('main', {
-    init:function(){
-      this.el.setObject3D('main', this.main);
+  name: 'ARdemo',
+  data() {
+    return {
+      splines: null,
+      group: new THREE.Object3D(),
+      dataReady: false
     }
-  });
+  },
+  methods: {
+    async initData() {
+      // setup data
+      const gf = new G3dFile({
+        url: 'https://wangftp.wustl.edu/~dli/tmp/test.g3d'
+      })
+      const data = await gf.readDataGenome(200000)
+      this.splines = getSplines(data)
+      this.dataReady = true
+      console.log(this.splines)
     }
+  },
+  async created() {
+    console.log('created')
+    console.log('init data')
+    await this.initData()
+    console.log('init ready')
+  },
+  watch: {
+    dataReady(newData) {
+      if (newData) {
+        const that = this
+        AFRAME.registerComponent('mymesh', {
+          update: function() {
+            const group = new THREE.Object3D()
+            console.log(that.splines)
+            Object.keys(that.splines).forEach(chrom => {
+              const { spline, color } = that.splines[chrom]
+              const geometry = new THREE.TubeBufferGeometry(
+                spline,
+                2000,
+                0.2,
+                8,
+                false
+              )
+              const material = new THREE.MeshBasicMaterial({ color })
+              const mesh = new THREE.Mesh(geometry, material)
+              mesh.scale.set(0.01, 0.01, 0.01)
+              group.add(mesh)
+            })
+
+            // const material = new THREE.MeshBasicMaterial({ color: 'blue' })
+            // const geometry = new THREE.BoxGeometry(1, 1, 1)
+            // const cube = new THREE.Mesh(geometry, material)
+            // cube.position.x = 0
+            // cube.position.y = 0.5
+            // cube.position.z = 0
+            // const cube2 = cube.clone()
+            // cube2.position.x = 1
+            // cube2.position.y = 1
+            // group.add(cube)
+            // group.add(cube2)
+
+            group.position.x = 0
+            group.position.y = 0.5
+            group.position.z = 0
+
+            console.log(group)
+            this.el.setObject3D('mesh', group) //unique name for each object
+            console.log(this.el.object3DMap)
+          }
+        })
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
